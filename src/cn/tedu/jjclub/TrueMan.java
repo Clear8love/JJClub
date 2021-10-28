5bea968fbb4a77490ab72e9b6532d4cb9f40dd89
JLuminary <947225307@qq.com>
2021-10-28 16:10:20 +0800

    小明

--- src/cn/tedu/jjclub/TrueMan.java
+++ src/cn/tedu/jjclub/TrueMan.java
@@ -12,7 +12,7 @@ public class TrueMan { //真男人就填名字
         switch (a){
             case 0:s = "男帝";break;
             case 1:s = "一郎";break;//郎
-            case 2:s = "空";break;
+            case 2:s = "小明";break;
             case 3:s = "空";break;
             case 4:s = "空";break;
             case 5:s = "空";break;
