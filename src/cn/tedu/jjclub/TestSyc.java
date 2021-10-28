d9d81e8b65cf9e9dac2c00ba9e322a7f5bdee504
NANDI GUO <44260801+907300985@users.noreply.github.com>
2021-10-28 14:53:18 +0800

    update

--- src/cn/tedu/jjclub/TestSyc.java
+++ src/cn/tedu/jjclub/TestSyc.java
@@ -1,13 +1,16 @@
 package cn.tedu.jjclub;
 
+import java.util.Scanner;
+
 /**
  * @Date:2021/10/28 14:11
  * @Author:NANDI_GUO
  */
 public class TestSyc {
     public static void main(String[] args) {
-        while(true){
-            System.out.println("兄弟们，一起导起来！"); //NANDI的注释
-        }
+        System.out.println("兄弟们，输入0-7随便一个整数");
+        int a = new Scanner(System.in).nextInt();
+        TrueMan man = new TrueMan();
+        System.out.println(man.op(a));
     }
 }
---
+++ src/cn/tedu/jjclub/TrueMan.java
@@ -0,0 +1,24 @@
+package cn.tedu.jjclub;
+
+import java.util.Scanner;
+
+/**
+ * @Date:2021/10/28 14:41
+ * @Author:NANDI_GUO
+ */
+public class TrueMan { //真男人就填名字
+    String s = null;
+    public String op(int a){
+        switch (a){
+            case 0:s = "男帝";break;
+            case 1:s = "空";break;
+            case 2:s = "空";break;
+            case 3:s = "空";break;
+            case 4:s = "空";break;
+            case 5:s = "空";break;
+            case 6:s = "空";break;
+            case 7:s = "空";break;
+        }
+        return s;
+    }
+}
