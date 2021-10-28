b03a6239e19ad0ab731268cd92a1931e3fb37cd2
NANDI GUO <44260801+907300985@users.noreply.github.com>
2021-10-28 14:20:42 +0800

    FirstProject
    试一试同步效果

---
+++ src/cn/tedu/jjclub/TestSyc.java
@@ -0,0 +1,13 @@
+package cn.tedu.jjclub;
+
+/**
+ * @Date:2021/10/28 14:11
+ * @Author:NANDI_GUO
+ */
+public class TestSyc {
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("兄弟们，一起导起来！");
+        }
+    }
+}
