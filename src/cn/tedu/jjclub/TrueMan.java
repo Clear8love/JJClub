cefea4f18fa0136d0640cca2f57bc704be8b1630
G-Gary <93308881+G-Gary@users.noreply.github.com>
2021-10-28 16:07:05 +0800

    1

--- src/cn/tedu/jjclub/TrueMan.java
+++ src/cn/tedu/jjclub/TrueMan.java
@@ -11,7 +11,7 @@ public class TrueMan { //真男人就填名字
     public String op(int a){
         switch (a){
             case 0:s = "男帝";break;
-            case 1:s = "一郎";break;
+            case 1:s = "一郎";break;//郎
             case 2:s = "空";break;
             case 3:s = "空";break;
             case 4:s = "空";break;
