a70d2473c9d12110a2c6a5278d90b582b3ab783e
NANDI GUO <44260801+907300985@users.noreply.github.com>
2021-10-28 15:19:52 +0800

    Update TestSyc.java

--- src/cn/tedu/jjclub/TestSyc.java
+++ src/cn/tedu/jjclub/TestSyc.java
@@ -8,16 +8,13 @@ import java.util.Scanner;
  */
 public class TestSyc {
     public static void main(String[] args) {
-<<<<<<< Updated upstream
         System.out.println("兄弟们，输入0-7随便一个整数");
         int a = new Scanner(System.in).nextInt();
         TrueMan man = new TrueMan();
         System.out.println(man.op(a));
-=======
         while(true){
             System.out.println("兄弟们，一起导起来！");
 
         }
->>>>>>> Stashed changes
     }
 }
